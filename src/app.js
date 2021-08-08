88e4b9bb2cefdd53a75c5c5d225918235b36d4c5
Jennifer Holliday <jennifer.holliday@gmail.com>
2021-08-07 19:33:08 -0600

    Added JS Date and Time

--- src/app.js
+++ src/app.js
@@ -1,1 +1,39 @@
+let date = new Date();
+let today = date.getDate();
 
+let hour = date.getHours();
+let ampm = hour >= 12 ? "pm" : "am";
+hour = hour % 12;
+hour = hour ? hour : 12; // the hour '0' should be '12'
+
+let minutes = date.getMinutes();
+minutes = minutes < 10 ? "0" + minutes : minutes;
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[date.getDay()];
+
+let months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+let month = months[date.getMonth()];
+
+let currentDateTime = document.querySelector("#time-date");
+currentDateTime.innerHTML = `${day}, ${month} ${today}  at ${hour}:${minutes} ${ampm}`;
